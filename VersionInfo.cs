6c4acc2117853082a056e4feae8ea4a984ea1e71
Ryan Parnell <ryan@parnellfamily.net>
2017-03-03 12:34:02 -0500

    Rework to allow for configuration and platform

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.13.1.0")]
-[assembly: AssemblyFileVersion("2.13.1.0")]
+[assembly: AssemblyVersion("2.13.2.*")]
+//[assembly: AssemblyFileVersion("2.13.2.0")]
